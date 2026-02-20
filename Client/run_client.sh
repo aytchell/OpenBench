07a21dffd2fee203037c0c41713b6fd8ffa6b838
Hannes Lerchl <hannes.lerchl@googlemail.com>
2026-02-20 10:08:56 +0100

    Add dummy http server to please healthiness probes

--- Client/run_client.sh
+++ Client/run_client.sh
@@ -9,6 +9,8 @@ if [ -z $ENV_PASSWD ]; then
     echo "Password is empty"
 else
     echo "Password given"
+    echo "Startin dummy http server"
+    python3 -m http.server 8080 &
 fi
 
 python3 client.py -U "$ENV_USER" -P "$ENV_PASSWD" -S http://aytchell.eu.pythonanywhere.com/ -T"$ENV_THREADS" -N1
